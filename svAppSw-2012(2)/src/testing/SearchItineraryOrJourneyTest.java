348c6e4007c0d1784446644ba6d65525157ee00e
Alessandro <utente@utente-PC>
2014-10-05 09:57:52 +0200

    junit testing

--- svAppSw-2012(2)/src/testing/SearchItineraryOrJourneyTest.java
+++ svAppSw-2012(2)/src/testing/SearchItineraryOrJourneyTest.java
@@ -4,11 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import resources.ServiceDB;
+
 public class SearchItineraryOrJourneyTest {
 
 	@Test
 	public void testSearchItinerary() {
-		fail("Not yet implemented");
+		assertEquals("torino", ServiceDB.searchItinerary("torino", "", 0, "", "").getElencoItinerari().get(0).getStartLoc());
+		assertEquals("torino", ServiceDB.searchItinerary("torino", "roma", 0, "", "").getElencoItinerari().get(0).getStartLoc());
 	}
 
 }
